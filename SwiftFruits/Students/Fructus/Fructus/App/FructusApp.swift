fb17ce55fef57e516a1d7d5f94758e42c1a11b97
Dan T <dp.tyler@hotmail.co.uk>
2021-04-12 15:54:20 +0100

    Progress so far saved. Images and Icons done. Model started and data model started.

---
+++ SwiftFruits/Students/Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Dan Tyler on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
