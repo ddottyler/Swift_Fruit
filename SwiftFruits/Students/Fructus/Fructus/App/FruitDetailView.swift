d1a6507d2750f454bb95c16b001b83e27d3bc279
Dan T <dp.tyler@hotmail.co.uk>
2021-04-13 09:48:15 +0100

    FruitDetailView updated with title, headline, subheadline, and description

--- SwiftFruits/Students/Fructus/Fructus/App/FruitDetailView.swift
+++ SwiftFruits/Students/Fructus/Fructus/App/FruitDetailView.swift
@@ -18,9 +18,31 @@ struct FruitDetailView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false){
                 VStack(alignment: .center, spacing: 20) {
-                    VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
+                    // HEADER
+                    VStack(alignment: .leading, spacing: 20) {
+                        // TITLE
                         Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        // HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        // NUTRIENTS
+                        
+                        // SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        // DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        // LINK
+                        
                     } //:VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
                 } //:VSTACK
             } //: SCROLL
         } //: NAVIGATION
